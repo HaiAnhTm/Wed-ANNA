d424d3a6943c6307c12ba43e20ef56b6a4e79ec4
Haianhtm112 <157443099+HaiAnhTm@users.noreply.github.com>
2024-04-23 11:18:23 +0700

    Haianh (#42)
    * update info

--- Utils/CurrencyUtils.cs
+++ Utils/CurrencyUtils.cs
@@ -3,5 +3,6 @@
     public class CurrencyUtils
     {
         public static string CurrencyConvertToString(long? donGia) => $"{donGia:N0} VND" ?? string.Empty;
+        public static string CurrencyConvertToStringno(long? donGia) => $"{donGia:N0}" ?? string.Empty;
     }
 }
